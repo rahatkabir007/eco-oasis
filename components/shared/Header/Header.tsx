4ac88b2aa72445998b2ca6fc45d2f76ea5e05926
rahatkabir007 <rahatkabir04@gmail.com>
2023-02-07 21:36:35 +0600

    d

--- components/shared/Header/Header.tsx
+++ components/shared/Header/Header.tsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import { controller } from '../../../src/state/StateController'
 import { useTheme } from 'next-themes';
+import css from "./Header.module.css"
+import Link from 'next/link';
 
 interface Props {
 }
@@ -11,11 +13,10 @@ const Header: React.FC<Props> = (props) => {
     const states = useSelector(() => controller.states)
     let [open, setOpen] = useState(false);
     const { systemTheme, theme, setTheme } = useTheme();
-    // const [mounted, setMounted] = useState(false);
     const [nav, setNav] = useState(false);
 
     //Tailwind Classes
-    const genericHamburgerLine = `h-1 w-7  rounded-full bg-black transition ease transform duration-500`;
+    const genericHamburgerLine = `h-1 w-7 rounded-full bg-black dark:bg-white transition ease transform duration-500`;
 
     useEffect(() => {
         controller.setState({ mounted: true });
@@ -35,23 +36,44 @@ const Header: React.FC<Props> = (props) => {
     window.addEventListener('scroll', changeBackground)
 
     let Links = [
-        { name: "HOME", link: "/" },
-        { name: "SERVICE", link: "/" },
-        { name: "ABOUT", link: "/" },
-        { name: "BLOG'S", link: "/" },
-        { name: "CONTACT", link: "/" },
+        { name: "Home", link: "/" },
+        { name: "About", link: "/about" },
+        { name: "Product", link: "/products" },
+        { name: "Blogs", link: "/blogs" },
+        { name: "Contact", link: "/contact" },
     ];
 
 
     return (
         <div className={`w-full sticky top-0 left-0 ${nav ? 'bg-ecobglightblack' : 'bg-transparent'}`}>
-            <div className={`flex items-center justify-between py-4 md:px-10 px-7 container-x ${nav ? 'bg-ecobglightblack' : 'bg-transparent'}`}>
-                <div className='font-bold text-2xl cursor-pointer flex items-center font-[Poppins] text-gray-800'>
-                    <span className='text-3xl text-indigo-600 mr-1'>
-                        Designer
-                    </span>
-
+            <div className={`flex items-center justify-between py-6 md:px-10 px-7 container-x ${nav ? 'bg-ecobglightblack' : 'bg-transparent'}`}>
+                <div className='cursor-pointer flex items-center font-[Poppins] gap-x-24'>
+                    <Link href="/"><div>
+                        <img src="/images/Logo.png" alt="logo"
+                            className={css["logo"]}
+                        />
+                    </div></Link>
+                    <div>
+                        <ul className={`md:flex md:items-center gap-x-20 md:pb-0 sm:bg-transparent bg-white dark:bg-zinc-900 pb-12 absolute md:static md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-[85px]' : 'top-[-490px]'}`}>
+                            {
+                                Links.map((link) => (
+                                    <li key={link.name} className='md:mr-8 text-xl md:my-0 my-7'>
+                                        <a href={link.link} className='text-[#000000] hover:text-ecolightgreen dark:hover:text-ecolightgreen  duration-500 dark:text-white text-base capitalize font-medium'>{link.name}</a>
+                                    </li>
+                                ))
+                            }
+                            <div className={`${css["mobileLoginContainer"]} gap-5 mr-10`}>
+                                <div className='md:mr-8 text-xl md:my-0 my-7'>
+                                    <button className='border border-ecodarkgreen dark:border-white  rounded px-4 py-[2px] text-[#000] font-medium text-base dark:text-white hover:text-ecolightgreen dark:hover:text-ecolightgreen'>Login</button>
+                                </div>
+                                <div className=''>
+                                    <button className='border border-ecodarkgreen dark:border-white rounded px-3 py-[2px] text-[#000] font-medium text-base dark:text-white hover:text-ecolightgreen dark:hover:text-ecolightgreen'>Sign Up</button>
+                                </div>
+                            </div>
+                        </ul>
+                    </div>
                 </div>
+
                 <div className='flex justify-center items-center sm:flex-row flex-row-reverse'>
                     <div className="ml-4">
                         <button
@@ -77,17 +99,13 @@ const Header: React.FC<Props> = (props) => {
                             />
                         </button>
                     </div>
-                    <div>
-                        <ul className={`md:flex md:items-center md:pb-0 sm:bg-transparent bg-white pb-12 absolute md:static md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-16' : 'top-[-490px]'}`}>
-                            {
-                                Links.map((link) => (
-                                    <li key={link.name} className='md:mr-8 text-xl md:my-0 my-7'>
-                                        <a href={link.link} className='text-gray-800 hover:text-gray-400 duration-500 dark:text-white '>{link.name}</a>
-                                    </li>
-                                ))
-                            }
-
-                        </ul>
+                    <div className={`${css["desktopLoginContainer"]} gap-5 mr-10`}>
+                        <div className=''>
+                            <button className='border border-ecodarkgreen dark:border-white  rounded px-4 py-[2px] text-[#000] font-medium text-base dark:text-white hover:text-ecolightgreen dark:hover:text-ecolightgreen'>Login</button>
+                        </div>
+                        <div className=''>
+                            <button className='border border-ecodarkgreen dark:border-white rounded px-3 py-[2px] text-[#000] font-medium text-base dark:text-white hover:text-ecolightgreen dark:hover:text-ecolightgreen'>Sign Up</button>
+                        </div>
                     </div>
                     <div className="flex justify-center">
                         {currentTheme === 'dark' ? (
