da231948b6ece704b6bcca73509deb85fc467153
rahatkabir007 <rahatkabir04@gmail.com>
2023-02-09 21:44:47 +0600

    products added

--- components/pages/Products/Products.tsx
+++ components/pages/Products/Products.tsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
+import { IProducts } from '../../../interfaces/models'
 import { controller } from '../../../src/state/StateController'
+import { Jsondata } from '../../../src/utils/Jsondata'
 
 interface Props {
 }
@@ -8,10 +10,91 @@ interface Props {
 const Products: React.FC<Props> = (props) => {
 
     const states = useSelector(() => controller.states)
+    const [products, setProducts] = useState<Array<IProducts>>([])
+    const [filteredProducts, setFilteredProducts] = useState<Array<IProducts>>([])
+    const [selected, setSelected] = useState("All Products");
+
+
+    useEffect(() => {
+        fetch("/products.json")
+            .then(res => res.json())
+            .then(data => {
+                setProducts(data)
+                setFilteredProducts(data.filter((product: IProducts) => {
+                    return product.category.includes("All Products")
+                }))
+            })
+
+    }, [])
+
+    const handleProductsFilter = (item: string) => {
+        setSelected(item)
+        setTimeout(() => {
+            if (item === 'All Products') {
+                setFilteredProducts(products.filter((product) => product.category.includes("All Products")));
+            } else {
+                setFilteredProducts(products.filter((product) => product.category.includes(item)));
+            }
+        }, 500);
+    }
+
 
     return (
-        <div>
-            Products
+        <div className="container-x">
+            <div className='flex flex-col px-2 md:px-20 '>
+                <div className="flex">
+                    <div>
+                        <input type="text" placeholder='Search' className="outline-none border-2 border-[#00AD7F] text-[#92B1A4] rounded-l-lg py-[7px] px-2 w-[160px] md:w-[421px] dark:bg-transparent dark:text-white" />
+                    </div>
+                    <div>
+                        <select className="outline-none border-2 border-l-0 border-[#00AD7F] text-[#1c1c1c] text-sm py-[8px] px-2 w-[100px] md:w-[145px] dark:bg-transparent dark:text-white">
+                            <option value="none" selected disabled hidden className='text-[#1c1c1c] dark:bg-transparent dark:text-white'>All Categories</option>
+                            {
+                                Jsondata.productCategories.slice(1).map((item, ind) => {
+                                    return (
+                                        <option value={item} className='text-[#1c1c1c]'>{item}</option>
+                                    )
+                                })
+                            }
+                        </select>
+                    </div>
+                    <div>
+                        <button type='submit' className="text-base bg-ecolightgreen text-white font-medium px-5 py-[9px] rounded-r-lg">
+                            Search
+                        </button>
+                    </div>
+                </div>
+                <div className='flex flex-wrap gap-6 mt-8'>
+                    {
+                        Jsondata.productCategories.map((item, ind) => {
+                            return (
+                                <div>
+                                    <button key={item} onClick={() => handleProductsFilter(item)} className={`${selected === item ? "bg-ecolightgreen text-white" : "bg-transparent"} text-sm  border border-[#92B1A4] dark:text-white px-5 py-[9px] rounded-lg`}>{item}</button>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+                <div className="grid md:grid-cols-2 lg:grid-cols-3 grid-cols-1 gap-16 py-6 px-6 md:py-16 md:px-32">
+                    {
+                        filteredProducts.map((product, ind) => {
+                            return (
+                                <div className="flex justify-center items-center flex-col gap-y-3 bg-[#F7F7F7] py-10 rounded-lg">
+                                    <div>
+                                        <img src={product.image} alt="" className='w-[122px] h-[156px] object-cover' />
+                                    </div>
+                                    <div>
+                                        <p className="text-[#3B3B3B] font-bold text-lg">{product.name}</p>
+                                    </div>
+                                    <div>
+                                        <p className="text-[#3B3B3B] text-lg">${" "}{product.price}</p>
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+            </div>
         </div>
     )
 }
---
+++ src/API/EcoOasis.ts
@@ -0,0 +1,47 @@
+import { callFetch } from "../utils/CallFetch";
+
+
+export const API_ENDPOINT = process.env["NEXT_PUBLIC_API_ENDPOINT"];
+
+export interface LoginInterface {
+  status: number;
+  data: {
+    access_token: string | null;
+  };
+}
+
+export class EcoOasis {
+
+  //DEMO API CALLING STRUCTURE
+  // static async login(token: string, email: string, fullName: string, avatar: string, tokenType: "google" | "facebook"): Promise<ILoginResponse> {
+  //     console.log(token);
+  //     console.log(API_ENDPOINT)
+  //     const myHeaders = new Headers();
+  //     myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+  //     const urlencoded = new URLSearchParams();
+  //     urlencoded.append("token", token);
+  //     urlencoded.append("tokenType", tokenType);
+  //     urlencoded.append("email", email);
+  //     urlencoded.append("fullName", fullName);
+  //     urlencoded.append("avatar", avatar);
+  //     const requestOptions = {
+  //         method: 'POST',
+  //         headers: myHeaders,
+  //         body: urlencoded,
+  //         redirect: 'follow'
+  //     };
+
+  //     return await callFetch(`${API_ENDPOINT}/users/login`, requestOptions)
+  // }
+
+  static async fetchProducts(): Promise<any> {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    return await callFetch("/products.json", requestOptions)
+  }
+
+
+
+}
