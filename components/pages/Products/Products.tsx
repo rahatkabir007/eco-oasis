ad24fb089ba6a6258af068effe13dce9c0da40a2
rahatkabir007 <rahatkabir04@gmail.com>
2023-03-30 11:47:16 +0600

    d

--- components/pages/Products/Products.tsx
+++ components/pages/Products/Products.tsx
@@ -15,45 +15,9 @@ const Products: React.FC<Props> = (props) => {
 
     return (
         <div className="container-x">
-<<<<<<< HEAD
-            <div className='flex flex-col px-2 md:px-20 mt-10'>
-                <form className="flex" onClick={handleSearch}>
-                    <div>
-                        <input type="text" placeholder='Search' className="outline-none border-2 border-[#00AD7F] text-[#92B1A4] rounded-l-lg py-[7px] px-2 w-[160px] md:w-[421px] dark:bg-transparent dark:text-white" onChange={handleChange} />
-                    </div>
-                    <div>
-                        <select onChange={handleChange} className="outline-none border-2 border-l-0 border-[#00AD7F] text-[#1c1c1c] text-sm py-[8.5px] px-2 w-[100px] md:w-[145px] dark:bg-transparent dark:text-white">
-                            <option value="All Categories" selected className='text-[#1c1c1c] dark:bg-transparent dark:text-white'>All Categories</option>
-                            {
-                                Jsondata.productCategories.slice(1).map((item, ind) => {
-                                    return (
-                                        <option value={item} className='text-[#1c1c1c]'>{item}</option>
-                                    )
-                                })
-                            }
-                        </select>
-                    </div>
-                    <div>
-                        <button type='submit' className="text-base bg-ecolightgreen text-white font-medium px-5 py-[8.5px] rounded-r-lg">
-                            Search
-                        </button>
-                    </div>
-                </form>
-                <div className='flex flex-wrap gap-6 mt-8'>
-                    {
-                        Jsondata.productCategories.map((item, ind) => {
-                            return (
-                                <div>
-                                    <button key={item} onClick={() => handleProductsFilter(item)} className={`${selected === item ? "bg-ecolightgreen text-white" : "bg-transparent"} text-sm  border border-[#92B1A4] dark:text-white px-5 py-[9px] rounded-lg`}>{item}</button>
-                                </div>
-                            )
-                        })
-                    }
-=======
             <div className='flex flex-col items-center text-center w-full p-4 md:p-0 md:w-[85%] mx-auto my-12 md:my-24'>
                 <div>
                     <p className="text-xl text-ecodarkgreen font-semibold it">Products Range</p>
->>>>>>> 975aba1dcc9486ab2c8ff0ffec41d199f47c19ed
                 </div>
                 <div className='mt-[13px]'>
                     <p className="text-[15px] text-ecoblack dark:text-white">Eco Oasis Limited chooses to source and deliver eco-friendly biodegradable products in considering the sustainability prospect in the coming future that aligns with our business ethics and core values. Eco Oasis Limited ensures 100% biodegradable materials like Jut, Cotton, Bamboo, Cane, Seagrass, Leather, etc. through proper quality checks of standard testing. Our product range covers Rugs, Floor mat, Table Runner, Ladies' bags, Shopping Bags, Baskets, etc. made of Jute and Cotton. We also source and export Genuine Leather products like Men belts, Ladies' Purses, Bags, Wallets, Footwear, Jackets, Hand Gloves, etc.</p>
